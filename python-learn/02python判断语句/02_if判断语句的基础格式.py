a21b64c4ec794a5c6f2a71eb621ae3a7368e93dc
cds007 <2205175277@qq.com>
2024-06-01 20:54:19 +0800

    判断

---
+++ python-learn/02python判断语句/02_if判断语句的基础格式.py
@@ -0,0 +1,19 @@
+"""
+演示Python判断语句：if语句的基本格式应用
+"""
+age = 10
+
+if age >= 18:
+    print("我已经成年了")
+    print("即将步入大学生活")
+
+print("时间过的真快呀")
+
+print("案例练习题")
+print("")
+print("欢迎来到游乐园，儿童免费，成人收费")
+age = int(input("请输入年龄："))
+if age >= 18:
+    print("十块钱")
+else:
+    print("免费")
